dd02ce36357aa422adc37c781b16418281d528cb
Calvin Feng <calvin.j.feng@gmail.com>
2021-10-11 00:48:26 -0700

    Use JSON to keep track of downloaded

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/calvinfeng/m3u8/downloader"
 )
@@ -22,7 +24,19 @@ func init() {
 	flag.StringVar(&filename, "f", "", "Output filename, required")
 }
 
+type Task struct {
+	URL      string `json:"url"`
+	Dir      string `json:"dir"`
+	Filename string `json:"filename"`
+	Done     bool   `json:"done"`
+}
+
 func main() {
+	workDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
 	flag.Parse()
 	defer func() {
 		if r := recover(); r != nil {
@@ -30,28 +44,43 @@ func main() {
 			os.Exit(-1)
 		}
 	}()
-	if url == "" {
-		panicParameter("u")
-	}
-	if output == "" {
-		panicParameter("o")
-	}
-	if filename == "" {
-		panicParameter("f")
-	}
 	if chanSize <= 0 {
 		panic("parameter 'c' must be greater than 0")
 	}
-	downloader, err := downloader.New(output, url)
+
+	jsonData, err := os.ReadFile(filepath.Join(workDir, "resources.json"))
 	if err != nil {
 		panic(err)
 	}
-	if err := downloader.Start(chanSize, filename); err != nil {
+
+	tasks := make([]*Task, 0)
+	if err := json.Unmarshal(jsonData, &tasks); err != nil {
 		panic(err)
 	}
-	fmt.Println("Done!")
-}
 
-func panicParameter(name string) {
-	panic("parameter '" + name + "' is required")
+	for _, task := range tasks {
+		if task.Done {
+			fmt.Printf("skip %s\n", task.Filename)
+			continue
+		}
+		task.Dir = filepath.Join(workDir, "downloads")
+		downloader, err := downloader.New(task.Dir, task.URL)
+		if err != nil {
+			panic(err)
+		}
+		if err := downloader.Start(chanSize, task.Filename); err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s is done!", task.Filename)
+		task.Done = true
+	}
+
+	jsonData, err = json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := os.WriteFile(filepath.Join(workDir, "resources.json"), jsonData, 0777); err != nil {
+		panic(err)
+	}
 }
